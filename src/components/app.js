4350ce7e13bc95f9c89822d0898380be8b4bcd4d
aar.56aint <aaron.hays_7@yahoo.com>
2020-06-13 14:33:36 +0100

    defaultProps and PropTypes.isRequired

--- src/components/app.js
+++ src/components/app.js
@@ -4,12 +4,12 @@ import PropTypes from 'prop-types';
 const App = (props) => <h1>{`Hello ${props.name}`}</h1>;
 
 App.propTypes = {
-  name: PropTypes.string,
+  name: PropTypes.string.isRequired
 };
 
-App.defaultProps ={
+/*App.defaultProps ={
   name: 'World',
-};
+};*/
 
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import { render } from 'react-dom';
 import App from './components/app';
 
 
-render (<App name="Aaron" />, document.getElementById('root'));
+render (<App />, document.getElementById('root'));
 
 
