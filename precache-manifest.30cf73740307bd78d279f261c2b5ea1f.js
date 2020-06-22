e2415ed9e602002c1d56fd9ff96c09f170487cac
aar.56aint <aaron.hays_7@yahoo.com>
2020-06-22 23:29:36 +0100

    Updates

---
+++ precache-manifest.30cf73740307bd78d279f261c2b5ea1f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe5888627814c1a87023a40ca39016c0",
+    "url": "/my-weather-app/index.html"
+  },
+  {
+    "revision": "acb26d601f6888092f73",
+    "url": "/my-weather-app/static/css/2.774c586d.chunk.css"
+  },
+  {
+    "revision": "b685ab982e9c699cef3d",
+    "url": "/my-weather-app/static/css/main.751f8e26.chunk.css"
+  },
+  {
+    "revision": "acb26d601f6888092f73",
+    "url": "/my-weather-app/static/js/2.5d970971.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-weather-app/static/js/2.5d970971.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b685ab982e9c699cef3d",
+    "url": "/my-weather-app/static/js/main.02fa137d.chunk.js"
+  },
+  {
+    "revision": "320153617ae488acd0f8",
+    "url": "/my-weather-app/static/js/runtime-main.9bd7dffe.js"
+  },
+  {
+    "revision": "4f717805c563ac36ae7ae62cefa7a794",
+    "url": "/my-weather-app/static/media/my-weather-wallpaper-mobile.4f717805.jpg"
+  },
+  {
+    "revision": "a9f3ad49a2569ba3ea4daeeac93ec90a",
+    "url": "/my-weather-app/static/media/my-weather-wallpaper.a9f3ad49.jpg"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/my-weather-app/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/my-weather-app/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/my-weather-app/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/my-weather-app/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/my-weather-app/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  }
+]);
